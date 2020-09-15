727d3820d42a7e71fa601a541b93a6a46cfe552a
Daniel Linder <dan@linder.org>
2020-09-15 21:05:49 +0000

    Misc work added

--- SetupK3SLab/main.tf
+++ SetupK3SLab/main.tf
@@ -1,4 +1,4 @@
-# Setup K3S lab VMs
+# Setup okd4 lab VMs
 # After this runs, use the inventory to setup with home lab script:
 # ansible-playbook -i inventory ~/SetupHomeLab/main.yml -u root
 
@@ -36,7 +36,7 @@ variable "guest_vm_ssh_user" { type = string }
 variable "guest_vm_ssh_port" { type = number }
 variable "guest_vm_ssh_passwd" { type = string }
 variable "hn_to_ip" { type = map }
-variable "hn_to_k3smac" { type = map }
+variable "hn_to_okd4mac" { type = map }
 variable "hn_to_homemac" { type = map }
 variable "hn_to_nm" { type = map }
 variable "hn_to_gw" { type = map }
@@ -64,7 +64,7 @@ resource "esxi_guest" "okd4_bootstrap" {
   }
 
   network_interfaces {
-    mac_address     = var.hn_to_k3smac["okd4_bootstrap"]
+    mac_address     = var.hn_to_okd4mac["okd4_bootstrap"]
     virtual_network = var.home_network
     nic_type        = "vmxnet3"
     # NOTE: the ipv4_address/_gateway are not supported with esxi.
@@ -88,7 +88,7 @@ resource "esxi_guest" "okd4_services" {
   }
 
   network_interfaces {
-    mac_address     = var.hn_to_k3smac["okd4_services"]
+    mac_address     = var.hn_to_okd4mac["okd4_services"]
     virtual_network = var.home_network
     nic_type        = "vmxnet3"
     # NOTE: the ipv4_address/_gateway are not supported with esxi.
@@ -112,7 +112,7 @@ resource "esxi_guest" "okd4_pfsense" {
   }
 
   network_interfaces {
-    mac_address     = var.hn_to_k3smac["okd4_pfsense"]
+    mac_address     = var.hn_to_okd4mac["okd4_pfsense"]
     virtual_network = var.home_network
     nic_type        = "vmxnet3"
     # NOTE: the ipv4_address/_gateway are not supported with esxi.
@@ -136,7 +136,7 @@ resource "esxi_guest" "okd4_control_plane" {
   network_interfaces {
     virtual_network = var.home_network
     nic_type        = "vmxnet3"
-    mac_address     = var.hn_to_k3smac["okd4_control_plane_${count.index}"]
+    mac_address     = var.hn_to_okd4mac["okd4_control_plane_${count.index}"]
   }
 }
 
@@ -155,7 +155,7 @@ resource "esxi_guest" "okd4_compute" {
   network_interfaces {
     virtual_network = var.home_network
     nic_type        = "vmxnet3"
-    mac_address     = var.hn_to_k3smac["okd4_compute_${count.index}"]
+    mac_address     = var.hn_to_okd4mac["okd4_compute_${count.index}"]
   }
 }
 
